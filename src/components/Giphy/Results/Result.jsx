1e768d653fe6942cf99aa17d986691476e28ce4b
Max <maximarnayt@gmail.com>
2019-11-08 15:48:02 +0200

    add mobile styles for giphy

--- src/components/Giphy/Results/Result.jsx
+++ src/components/Giphy/Results/Result.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Result.scss';
 
 const Result = ({ result }) => {
   console.log(result);
   return (
     <>
       <img
+        className="giphy__img"
         src={result.images.downsized_large.url}
         alt="img"
       />
