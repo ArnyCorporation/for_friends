5eaa6edaa0ab412e70caec22de5d46139c06235a
Max <maximarnayt@gmail.com>
2019-11-08 13:53:26 +0200

    Updates

---
+++ precache-manifest.5e1b5fbe770d39d98e1dda53d6dcc8fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d7e9bdf8465db291f99873dd2a89d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c534b72785da584ba6",
+    "url": "/static/css/main.e490d77a.chunk.css"
+  },
+  {
+    "revision": "c7fadd0177b715eb5b22",
+    "url": "/static/js/2.b7b7fa09.chunk.js"
+  },
+  {
+    "revision": "56c534b72785da584ba6",
+    "url": "/static/js/main.5f1c430b.chunk.js"
+  },
+  {
+    "revision": "95556449c92ebd94b007",
+    "url": "/static/js/runtime-main.db26f754.js"
+  }
+]);
