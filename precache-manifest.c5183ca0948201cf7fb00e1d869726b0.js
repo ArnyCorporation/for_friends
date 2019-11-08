41669608639d87b7ac3a59635cfcd4bef507ccb8
Max <maximarnayt@gmail.com>
2019-11-08 14:40:49 +0200

    Updates

--- precache-manifest.c5183ca0948201cf7fb00e1d869726b0.js
+++ precache-manifest.c5183ca0948201cf7fb00e1d869726b0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "670ba51b4a33933e6c913658b3711472",
+    "revision": "bfe517dde683a858fa74e6bc7315b14b",
     "url": "/for_friends/index.html"
   },
   {
-    "revision": "c759dc9f2795572d1596",
+    "revision": "fadd6d4bfd6e896f0761",
     "url": "/for_friends/static/css/main.e490d77a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/for_friends/static/js/2.892fcf4e.chunk.js"
   },
   {
-    "revision": "c759dc9f2795572d1596",
-    "url": "/for_friends/static/js/main.4ff7c288.chunk.js"
+    "revision": "fadd6d4bfd6e896f0761",
+    "url": "/for_friends/static/js/main.467a69ae.chunk.js"
   },
   {
     "revision": "eb089f3459a5b68983ab",
